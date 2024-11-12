e3556d4869b4fe8e35dd8e44c417b3b92203119d
enricobrum <e.brunelli010@studenti.unibs.it>
2024-11-12 14:33:55 +0000

    Programmi di utilizzo per l'avvio del client e del server

---
+++ Test_Client_Server/Lettura_File_Config.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#Funzione per leggere da file di configurazione
+declare -A inidb                        #creazione array in Bash
+function _ini_get_section {
+    if [[ "$1" =~ ^(\[)(.*)(\])$ ]]; 
+    then 
+        echo ${BASH_REMATCH[2]} ; 
+    else 
+        echo ""; 
+    fi
+}                                       #permette di estrarre la sezione dal file di configurazione
+function _ini_get_key_value {
+    if [[ "$1" =~ ^([^=]+)=([^=]+)$ ]]; 
+    then 
+        echo "${BASH_REMATCH[1]}=${BASH_REMATCH[2]}"; 
+    else 
+        echo ""
+    fi
+}                                       #permette di estrarre la chiave dal file di configurazione
+function ini_printdb {
+    for i in "${!inidb[@]}"
+    do
+    # split the associative key in to section and key
+       echo -n "section  : $(echo $i | cut -f1 -d ' ');"
+       echo -n "key  : $(echo $i | cut -f2 -d ' ');"
+       echo  "value: ${inidb[$i]}"
+    done
+}                                       #stampa l'intero file di configurazione
+function ini_get_value {
+    section=$1
+    key=$2
+    echo "${inidb[$section $key]}"
+}                                       #permette di estrarre il valore relativo alla SEZIONE e CHIAVE
+function ini_loadfile {
+    local cur_section=""
+    local cur_key=""
+    local cur_val=""
+    IFS=
+    while read -r line; do
+        new_section=$(_ini_get_section $line)
+     #    got a new section
+        if [[ -n "$new_section" ]]; then
+            cur_section=$new_section
+        # not a section, try a key value
+        else
+            val=$(_ini_get_key_value $line)
+            # trim the leading and trailing spaces as well
+            cur_key=$(echo $val | cut -f1 -d'=' | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//') 
+            cur_val=$(echo $val | cut -f2 -d'=' | sed -e 's/^[[:space:]]*//' | sed -e 's/[[:space:]]*$//')
+        if [[ -n "$cur_key" ]]; then
+            # section + key is the associative in bash array, the field seperator is space
+            inidb[${cur_section} ${cur_key}]=$cur_val
+        fi
+    fi
+    done <$1
+}                                       #funzione utilizzata per caricare il file di configurazione in modo da utilizzare poi le varie funzioni realizzate
+ini_loadfile config.ini
---
+++ Test_Client_Server/Start_Server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Programma utilizzato per l'avvio dei server da lato core 5G posti 
+#sul nodo centrale della rete 5G Firecell. Inoltre e' anche configurato
+#l'applicazione wireshark al fine di ottenere i valori dei time-stamp 
+#relativi ai pacchetti scambiati nella fase di test della rete.
+#_______________________________________________________________________________
+#Importo i file contenenti le funzioni utilizzate per avviare e controllare
+#i Client e server, la lettura del file di configurazione,
+#la configurazione di client e server iperf e la configurazione dell'ascolto
+#effettuato da wireshark.
+chmod +x Lettura_File_Config.sh 
+source Lettura_File_Config.sh #Lettura del file di configurazione contenenti
+                              #i valori utilizzati per i test
+#_______________________________________________________________________________
+ip_server=$(ini_get_value server ip) #indirizzo IP del server di echo
+port_tcp=$(ini_get_value server port_tcp) #port in cui si pone il server di echo
+port_udp=$(ini_get_value server port_udp)
+#avvio del server con i parametri ottenuti da file di configurazione
+python3 Server.py --server_host "$ip_server" --tcp_port "$port_tcp" --udp_port "$port_udp" 
+wait $!
+
