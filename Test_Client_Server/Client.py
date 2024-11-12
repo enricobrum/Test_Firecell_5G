de429673e4196d499753c99d6387f9c0ff3e98a6
enricobrum <e.brunelli010@studenti.unibs.it>
2024-11-12 14:31:56 +0000

    Inizio definizione finale dei test

---
+++ Test_Client_Server/Client.py
@@ -0,0 +1,161 @@
+# Autore: Enrico Brunelli
+# Data Creazione: 12/11/2024
+# Data Ultima Modifica: 12/11/2024
+# 
+# Titolo: "Client per test su Firecell Labkit"
+#____________________________________________________________
+
+# Librerie utilizzate 
+import ntplib # Libreria per l'utilizzo di server NTP 
+import socket 
+import argparse # Permette di avviare il programma passando
+                # dei parametri 
+from datetime import datetime # Permette di riconoscere il 
+                              # formato dei timestamp
+#___________________________________________________________  
+# Funzione per l'ottenimento del timestamp dal server NTP
+def get_ntp_timestamp(ntp_client): # ntp_client: oggetto per 
+                                   # la comunicazione con il 
+                                   # server
+    server = 'ntp1.inrim.it' # Nome di del server 
+                             # NTP di riferimento
+    try:
+        response = ntp_client.request(server, version=3,
+                                      timeout=1)
+    except Exception as e:
+        print(f"Errore nella connessione:{e}")
+        response = 0
+    return response 
+#___________________________________________________________    
+# Funzione che implementa il test tramite connessione TCP
+def test_tcp(client_socket,ntp_client):
+    # Richiesta al server NTP prima di mandare il messaggio 
+    # di test
+    payload_size = 10 # Dimensione in Byte del messaggio 
+                      # pari a quella del pacchetto di 
+                      # risposta del server NTP per 
+                      # mantenere la coerenza
+    client_send_timestamp = get_ntp_timestamp(ntp_client)
+    if client_send_timestamp != 0: # Se la richiesta al 
+                                   # al server è andata a
+                                   # buon fine
+        message = "X" * payload_size # Generazione di un
+                                     # pacchetto da 10 Byte
+        client_socket.sendall(message.encode('utf-8'))
+        response = client_socket.recv(1024)
+        client_recv_timestamp = get_ntp_timestamp(ntp_client)
+        if client_recv_timestamp != 0:
+            return client_send_timestamp.tx_time,client_recv_timestamp.tx_time
+    else:
+        client_send_timestamp = 0
+        client_recv_timestamp = 0
+        return client_send_timestamp, client_recv_timestamp
+#___________________________________________________________    
+#Funzione per la connessione TCP con il server
+def connect_to_server(host, port):
+    """
+    Crea e stabilisce una connessione TCP con il server specificato.
+
+    Args:
+        host (str): Indirizzo IP del server.
+        port (int): Porta su cui il server e' in ascolto.
+
+    Returns:
+        socket: Oggetto socket connesso al server.
+    """
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f"Connesso al server {host}:{port}")
+    return client_socket
+#_____________________________________________________________
+#Funzione di test che permette l'invio di messaggi UDP.
+def test_udp(host, port, ntp_client):
+    """
+    Esegue un test UDP inviando pacchetti al server e ricevendo risposte.
+
+    Args:
+        host (str): Indirizzo IP del server.
+        port (int): Porta su cui il server UDP e' in ascolto.
+        file (File): File .csv per il salvataggio dei dati
+        traffic (str): scenario di traffico del test corrente 
+    """
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_send_timestamp = get_ntp_timestamp(ntp_client)
+    udp_socket.sendto(client_send_timestamp.encode(), (host, port))
+    response = udp_socket.recvfrom(1024)
+    client_recv_timestamp = get_ntp_timestamp(ntp_client)
+    udp_socket.close()
+    return client_recv_timestamp.tx_time,client_send_timestamp.tx_time
+#_____________________________________________________________________
+# Programma principale che permette la gestione del test, le connessioni 
+# con i vari protocolli e la ricezione di parametri in ingresso all'avvio
+def run_test_cycle(host, tcp_port, udp_port, interval, traffic, payload):
+    """
+    Esegue un ciclo di test di rete per ciascun intervallo specificato.
+
+    Args:
+        host (str): Indirizzo IP del server.
+        tcp_port (int): Porta del server TCP.
+        udp_port (int): Porta del server UDP.
+        traffic (str): Scenario di traffico del test corrente
+    """
+    n_test=1000
+    ntp_client =ntplib.NTPClient()
+    data_corrente = datetime.now()
+    data_stringa = data_corrente.strftime("%Y-%m-%d")
+    filecsv="istanti_temporali_"+data_stringa+".csv"
+    file=open(filecsv,"a")
+    if file.tell()==0:
+        file.write("Protocollo,Traffico,client_send_timestamp,client_recv_timestamp\n")
+        print("File csv creato.")
+    while True:
+        print("\nSeleziona un test da eseguire:")
+        print("1. Protocollo TCP")
+        print("2. Protocollo UDP")
+        print("3. Terminazione del test.")
+        scelta = input("Inserisci il numero del test da eseguire: ")
+        if scelta == '1':
+            print("\nEsecuzione Test con protocollo TCP:")
+            client_socket = connect_to_server(host, tcp_port)
+            try:
+                for _ in n_test:
+                    client_send_timestamp,client_recv_timestamp = test_tcp(client_socket,ntp_client)
+                    file.write('TCP'+','+traffic+','+client_send_timestamp+','+client_recv_timestamp+'\n')
+            finally:
+                client_socket.close()
+                print("Connessione al server chiusa dopo l'echo test")
+
+        elif scelta == '2':
+            print("\nEsecuzione Test con protocollo UDP:")
+            for _ in n_test:
+                    client_send_timestamp,client_recv_timestamp = test_udp(host,udp_port,ntp_client)
+                    file.write('UDP'+','+traffic+','+client_send_timestamp+','+client_recv_timestamp+'\n')
+        elif scelta == '3':
+            file.close()
+            print("Uscita dal programma.")
+            break
+        else:
+            print("Selezione non valida. Per favore, riprova.")
+            
+#Funzione principale che all'avvio del programma tramite l'oggetto "parser" consente di eseguire il codice
+#specificando gli argomenti necessari alla realizzazione del test. Dopo aver aggiunto tutti gli argomenti
+#passati al lancio del programma python, viene eseguita la funzione "run_test_cycle" e si entra nella fase
+#di test. 
+if __name__ == "__main__":
+    """
+        server_host: Indirizzo IP del server
+        tcp_port: Porta del server TCP
+        udp_port: Porta del server UDP
+        intervals: Intervalli di tempo tra i messaggi
+        traffic: Scenario di traffico del test
+        payload: Dimensioni crescenti del payload per test
+    
+    """
+    parser = argparse.ArgumentParser(description='Client TCP e UDP per il test della connessione.')
+    parser.add_argument('--server_host', type=str, required=True, help='Indirizzo IP del server')
+    parser.add_argument('--tcp_port', type=int, required=True, help='Porta del server TCP')
+    parser.add_argument('--udp_port', type=int, required=True, help='Porta del server UDP')
+    parser.add_argument('--traffic', type=str, required=True, help="Scenario di traffico del test")
+    args = parser.parse_args()
+
+    run_test_cycle(args.server_host, args.tcp_port, args.udp_port, args.traffic)
---
+++ Test_Client_Server/Server.py
@@ -0,0 +1,121 @@
+# Autore: Enrico Brunelli
+# Data Creazione: 12/11/2024
+# Data Ultima Modifica: 12/11/2024
+# 
+# Titolo: "Client per test su Firecell Labkit"
+#____________________________________________________________
+
+# Librerie utilizzate 
+import ntplib # Libreria per l'utilizzo di server NTP 
+import socket # Libreria per la gestione delle comunicaioni
+import threading # Permette la gestione di più funzioni 
+                # contemporaneamente
+import argparse # Permette di avviare il programma passando
+                # dei parametri 
+from datetime import datetime # Permette di riconoscere il 
+                              # formato dei timestamp
+#___________________________________________________________  
+# Funzione per l'ottenimento del timestamp dal server NTP
+def get_ntp_timestamp(ntp_client): # ntp_client: oggetto per 
+                                   # la comunicazione con il 
+                                   # server
+    server = 'ntp1.inrim.it' # Nome di del server 
+                             # NTP di riferimento
+    try:
+        response = ntp_client.request(server, version=3,
+                                      timeout=1)
+    except Exception as e:
+        print(f"Errore nella connessione:{e}")
+        response = 0
+    return response 
+#___________________________________________________________
+# Funzione per la gestione della connessione TCP
+def handle_tcp_connection(server_socket,client_address,ntp_client):
+    """
+    Gestisce una connessione TCP con il client.
+
+    Args:
+        client_socket (socket): Il socket connesso al client.
+        client_address (tuple): L'indirizzo del client.
+        ntp_client : Oggetto client ntp per la richiesta al server
+    """
+    print(f"TCP Connection from {client_address}")
+    try:
+        while True:
+            data = server_socket.recvfrom(1024)
+            if data != 0:
+                # Ottieni il timestamp NTP attuale
+                server_recv_timestamp = get_ntp_timestamp(ntp_client)
+                if server_recv_timestamp != 0:
+                    # Risponde al client con il timestamp del server
+                    server_send_timestamp = get_ntp_timestamp(ntp_client)
+                    server_socket.sendto(str(server_send_timestamp.tx_time).encode(), client_address)
+                else:
+                    server_socket.sendto(str("0").encode(), client_address)
+    except Exception as e:
+        print(f"Errore nella connessione TCP: {e}")
+    finally:
+        server_socket.close()
+        print(f"Connessione TCP chiusa con: {client_address}")
+    return server_recv_timestamp.tx_time,server_send_timestamp.tx_time
+#_______________________________________________________________________
+#Funzione per l'avvio del server TCP sull'indirizzo IP e porta passati come 
+#argomenti, rispettivamente, host e port
+def tcp_server(host, port):
+    """
+    Avvia un server TCP.
+
+    Args:
+        host (str): Indirizzo IP su cui il server e' in ascolto.
+        port (int): Porta su cui il server e' in ascolto.
+    """
+    ntp_client = ntplib.NTPClient()
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(5)
+    print(f"Server TCP in ascolto su {host}:{port}")
+
+    try:
+        while True:
+            client_socket, client_address = server_socket.accept()
+            threading.Thread(target=handle_tcp_connection, args=(client_socket, client_address, ntp_client)).start()
+    except KeyboardInterrupt:
+        print("\nArresto del server TCP.")
+    finally:
+        server_socket.close()
+#_____________________________________________________________________________
+#Funzione per l'avvio del server UDP sull'indirizzo IP e porta passati come 
+#argomenti, rispettivamente, host e port
+def udp_server(host, port):
+    """
+    Avvia un server UDP.
+
+    Args:
+        host (str): Indirizzo IP su cui il server e' in ascolto.
+        port (int): Porta su cui il server e' in ascolto.
+    """
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.bind((host, port))
+    print(f"Server UDP in ascolto su {host}:{port}")
+
+    try:
+        while True:
+            data, client_address = udp_socket.recvfrom(65536)
+            print(f"Ricevuto messaggio da {client_address}: {data.decode()}")
+            udp_socket.sendto(data, client_address)
+    except KeyboardInterrupt:
+        print("\nArresto del server UDP.")
+    finally:
+        udp_socket.close()
+#__________________________________________________________________________________
+# Funzione principale per l'avvio del programma server
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Client TCP e UDP per il test della connessione.')
+    parser.add_argument('--server_host', type=str, required=True, help='Indirizzo IP del server')
+    parser.add_argument('--tcp_port', type=int, required=True, help='Porta del server TCP')
+    parser.add_argument('--udp_port', type=int, required=True, help='Porta del server UDP')
+
+    args = parser.parse_args()
+
+    threading.Thread(target=tcp_server, args=(args.server_host, args.tcp_port)).start()
+    threading.Thread(target=udp_server, args=(args.server_host, args.udp_port)).start()
