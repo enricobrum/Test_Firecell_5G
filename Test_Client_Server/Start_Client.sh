a8b8c4bdb90eeb78988aaf575229fc8cd5bafb80
enricobrum <e.brunelli010@studenti.unibs.it>
2024-11-13 13:52:00 +0000

    c

--- Test_Client_Server/Start_Client.sh
+++ Test_Client_Server/Start_Client.sh
@@ -5,8 +5,9 @@ source Lettura_File_Config.sh
 ip_server=$(ini_get_value server ip)
 port_tcp=$(ini_get_value server port_tcp)
 port_udp=$(ini_get_value server port_udp)
+intervals=$(ini_get_value client intervals)
 traffic=$(ini_get_value client traffic)
-echo $port_tcp
+payload=$(ini_get_value client payload)
 #____________________________________________
 python3 Client.py --server_host "$ip_server" --tcp_port "$port_tcp" --udp_port "$port_udp" --traffic "$traffic"
 wait $!
